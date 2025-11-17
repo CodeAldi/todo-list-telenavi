9ad1a9adc9dc3de31aa5219bea3b46e35398316b
aldi-computer-home <codealdi@gmail.com>
2025-11-17 17:39:15 +0700

    add status and priority to fillable

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     //
+
+    protected $fillable = ['title','assignee','due_date','time_tracked','status','priority'];
+
+    protected function casts(): array
+    {
+        return [
+            'due_date' => 'datetime',
+            'time_tracked' => 'integer',
+            'status' => \App\Enums\StatusTodo::class,
+            'priority' => \App\Enums\PriorityTodo::class,
+        ];
+    }
 }
