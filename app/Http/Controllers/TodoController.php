8cafe9d60e0923c9759386d7e6dd48b9bf883249
aldi-computer-home <codealdi@gmail.com>
2025-11-17 17:43:40 +0700

    Crud logic for todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TodoController extends Controller
 {
@@ -11,7 +13,8 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::all();
+        return response()->json($todos);
     }
 
     /**
@@ -19,7 +22,24 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'assignee' => 'string',
+            'due_date' => ['required','date',Rule::date()->afterOrEqual('now')],
+            'time_tracked' => 'numeric',
+            'status' => [Rule::enum(\App\Enums\StatusTodo::class)],
+            'priority' => [Rule::enum(\App\Enums\PriorityTodo::class)],
+        ]);
+        $todo = new Todo();
+        $todo->title = $validatedData['title'];
+        $todo->assignee = $validatedData['assignee'] ?? null;
+        $todo->due_date = $validatedData['due_date'];
+        $todo->time_tracked = $validatedData['time_tracked'] ?? 0;
+        $todo->status = $validatedData['status'] ?? \App\Enums\StatusTodo::Pending;
+        $todo->priority = $validatedData['priority'] ?? \App\Enums\PriorityTodo::Medium;
+        
+        $todo->save();
+        return response()->json($todo, 201);
     }
 
     /**
@@ -27,7 +47,8 @@ class TodoController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+        return response()->json($todo);
     }
 
     /**
@@ -35,7 +56,21 @@ class TodoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'title' => 'sometimes|required|string',
+            'assignee' => 'sometimes|string',
+            'due_date' => 'sometimes|required|date|after_or_equel:now',
+            'time_tracked' => 'sometimes|numeric',
+            'status' => [Rule::enum(\App\Enums\StatusTodo::class)],
+            'priority' => [Rule::enum(\App\Enums\PriorityTodo::class)],
+        ]);
+
+        $todo->fill($validatedData);
+        $todo->save();
+
+        return response()->json($todo);
     }
 
     /**
@@ -43,6 +78,8 @@ class TodoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+        return response()->json(null, 204);
     }
 }
